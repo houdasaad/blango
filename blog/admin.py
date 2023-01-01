94fd4cfdfc72bc8a74860b76c167a0ddd6caecc9
Mehdi Zapata <jmzv13@gmail.com>
2023-01-01 14:26:20 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
-admin.site.register(Tag)
+
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    
-admin.site.register(Post, PostAdmin)
 
+# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
     
 
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 # other imports ommited
 
 
@@ -14,29 +17,6 @@ class Tag(models.Model):
     def __str__(self):
         return self.value
 
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    published_at = models.DateTimeField(blank=True, null=True)
-    title = models.TextField(max_length=100)
-    slug = models.SlugField()
-    summary = models.TextField(max_length=500)
-    content = models.TextField()
-    tags = models.ManyToManyField(Tag, related_name="posts")
-    comments = GenericRelation(Comment)
-    
-    def __str__(self):
-        return self.title
-
-class Comment(models.Model):
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
-
-
-
 
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
@@ -48,3 +28,23 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(blank=True, null=True)
+    title = models.TextField(max_length=100)
+    slug = models.SlugField()
+    summary = models.TextField(max_length=500)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="posts")
+    comments = GenericRelation(Comment)
+
+    
+    def __str__(self):
+        return self.title
